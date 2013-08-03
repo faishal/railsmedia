7b3af9583277e935bf97bcab6fc811c3400f1934
Faishal <saiyedfaishal@gmail.com>
2013-08-03 17:37:43 +0530

    init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsmedia::Application.config.secret_token = '8e3e5255318b5d07a146eb109289a21c2da2a902070fc1d36de6f8fe8f4f3e49e2f786d350f4541f4db8afebdaf9f18aa204af32041409182a13abfd266a8ff0'
